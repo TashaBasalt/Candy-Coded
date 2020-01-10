44be2b7a8ac1d03f432173e67ec46095a54f1bc4
Tasha Zuniga <tasha@basalt.io>
2020-01-10 14:42:43 -0800

    adding blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Link, graphql } from "gatsby"
+
+import Layout from "../components/layout/layout"
+
+const Blog = (props) => (
+  <Layout>
+    {props.data.allArticle.edges.map(edge => (
+      <div key={edge.node.id}>
+        <h2>
+          {edge.node.title}
+        </h2>
+        <Link to={`/article/${edge.node.id}`}>
+          Link Test
+        </Link>
+      </div>
+    ))}
+  </Layout>
+)
+
+export const query = graphql`
+  {
+    allArticle {
+      edges {
+        node {
+          body
+          date
+          id
+          tags
+          title
+        }
+      }
+    }
+  }
+`;
+
+export default Blog
