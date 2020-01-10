b2dea49c53f5e9963465991f85035261254517a7
Tasha Zuniga <tasha@basalt.io>
2020-01-10 14:44:51 -0800

    template for dynamically rendered pages

---
+++ src/templates/articleTemplate.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Layout from "../components/layout/layout"
+
+const articleTemplate = () => {
+  return (
+    <Layout>
+      <div>
+        Article Page
+      </div>
+    </Layout>
+  );
+}
+
+export default articleTemplate;
